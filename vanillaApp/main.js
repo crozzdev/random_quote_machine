a0469a81fe40388725c8784af01d0caeb5b81f23
CrozzDev <crozzdev95@outlook.com>
2024-05-02 12:18:47 -0500

    Finished vanillaApp

--- vanillaApp/main.js
+++ vanillaApp/main.js
@@ -17,24 +17,61 @@ const URLQUOTES = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c95
 
 const $body = document.querySelector('body')
 const $quoteSpan = document.getElementById('quote')
+const $quoteContainer = document.getElementById('quote-container')
 const $authorSpan = document.getElementById('author')
 const $buttons = document.getElementsByClassName('btn')
 const $newQuoteButton = document.getElementById('change-quote')
+const $twitterLink = document.getElementById('twitter')
 
+const buttonsArray = Array.from($buttons)
 let quotesArr = []
+let quotesLength = 0
 
 fetch(URLQUOTES)
   .then((res) => res.json())
   .then((data) => {
     quotesArr = data.quotes
+    quotesLength = quotesArr.length
     showQuoteAuthor(0)
+    changeColor(0)
   })
   .catch((err) => { $quoteSpan.textContent = 'There was an error loading the quotes check the console'; console.log(err) })
 
 const showQuoteAuthor = (index) => {
+  $authorSpan.textContent = ''
+  $authorSpan.style.opacity = 0
+  $quoteContainer.style.opacity = 0
+
   const quote = quotesArr[index].quote
   const author = quotesArr[index].author
 
-  $quoteSpan.textContent = quote
-  $authorSpan.textContent = `- ${author}`
+  $twitterLink.href = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author)
+
+  setTimeout(() => {
+    // eslint-disable-next-line quotes
+    $quoteSpan.textContent = quote
+    $authorSpan.textContent = `- ${author}`
+    $authorSpan.style.opacity = 1
+    $quoteContainer.style.opacity = 1
+  }, 500)
 }
+
+const changeColor = (index) => {
+  const color = COLORS[index]
+  $body.style.backgroundColor = color
+  $quoteContainer.style.color = color
+  $authorSpan.style.color = color
+
+  buttonsArray.forEach(element => {
+    element.style.backgroundColor = color
+  })
+}
+
+$newQuoteButton.addEventListener('click', () => {
+  const randomQuote = Math.floor(Math.random() * quotesLength)
+  const randomColor = Math.floor(Math.random() * COLORS.length)
+  showQuoteAuthor(randomQuote)
+  setTimeout(() => {
+    changeColor(randomColor)
+  }, 500)
+})
