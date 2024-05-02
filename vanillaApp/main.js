28abcbca0c0215ceb3a00c9ffd4cdfa5330e79b9
CrozzDev <crozzdev95@outlook.com>
2024-05-02 12:34:15 -0500

    renamed some variables so they match instructions in readme

--- vanillaApp/main.js
+++ vanillaApp/main.js
@@ -16,12 +16,12 @@ const COLORS = [
 const URLQUOTES = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
 
 const $body = document.querySelector('body')
-const $quoteSpan = document.getElementById('quote')
+const $quoteSpan = document.getElementById('text')
 const $quoteContainer = document.getElementById('quote-container')
 const $authorSpan = document.getElementById('author')
 const $buttons = document.getElementsByClassName('btn')
-const $newQuoteButton = document.getElementById('change-quote')
-const $twitterLink = document.getElementById('twitter')
+const $newQuoteButton = document.getElementById('new-quote')
+const $twitterLink = document.getElementById('tweet-quote')
 
 const buttonsArray = Array.from($buttons)
 let quotesArr = []
@@ -32,7 +32,8 @@ fetch(URLQUOTES)
   .then((data) => {
     quotesArr = data.quotes
     quotesLength = quotesArr.length
-    showQuoteAuthor(0)
+
+    showQuoteAuthor(Math.floor(Math.random() * quotesLength))
     changeColor(0)
   })
   .catch((err) => { $quoteSpan.textContent = 'There was an error loading the quotes check the console'; console.log(err) })
